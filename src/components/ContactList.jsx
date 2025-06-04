7d9086b0eb523961f97a81f68ace623c0b606ed9
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-06-04 23:55:58 +0300

    Refactor ContactList component to correctly use Redux for fetching contacts and update rendering logic

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,13 +1,20 @@
+import { useSelector } from "react-redux";
+import { selectContacts } from "../redux/reducers/contact/slice";
+
 const ContactList = () => {
+  const contacts = useSelector()(selectContacts);
+
   return (
     <div className={"contact-list"}>
       <h2 className={"contact-list-title"}>Contacts</h2>
       <ul className={"contact-list-items"}>
-        {/* Here you would map over your contacts and render them */}
+        Here you would map over your contacts and render them
         {/* Example: */}
-        {/* {contacts.map(contact => (
-                    <li key={contact.id}>{contact.name} - {contact.phone}</li>
-                ))} */}
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name} - {contact.phone}
+          </li>
+        ))}
       </ul>
     </div>
   );
