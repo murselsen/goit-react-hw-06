03f0b2b05b0bfbe5fc5b0bbdac2badae929bec05
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-06-05 22:51:16 +0300

    Initialize contacts state with default contact entries

--- src/redux/reducers/contacts/slice.js
+++ src/redux/reducers/contacts/slice.js
@@ -3,7 +3,12 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 const initialValues = {
-  items: [],
+  items: [
+    { id: "id-1", name: "Rosie Simpson", phone: "4591256" },
+    { id: "id-2", name: "Hermione Kline", phone: "4438912" },
+    { id: "id-3", name: "Eden Clements", phone: "6451779" },
+    { id: "id-4", name: "Annie Copeland", phone: "227-91-26" },
+  ],
 };
 
 const contactsSlice = createSlice({
